5800c3fc0fb5a20d758e936e7c0d5314db46af89
Kyriakos Chalvatzis <kyriakos.halvatzis@gmail.com>
2024-06-08 23:20:01 +0300

    commenting done on both Wp, by UNIX legends

--- Application1.scala
+++ Application1.scala
@@ -5,12 +5,15 @@ object Application1 extends App
 {
 
 // local configuration
+  // spark session
     private val spark = SparkSession.builder.master("local[*]")
     .appName("Application Scenario 1: Reuters News Stories Analysis using Spark RDDs")
     .getOrCreate()
   private val hdfsPath = "hdfs://localhost:9000"
   //  FileSystem.setDefaultUri(spark.sparkContext.hadoopConfiguration, hdfsPath)
 
+
+  // hdfs input/output paths
   private val application1_path = hdfsPath + "/reuters"
 
   private val categories_documents_path = application1_path + "/rcv1-v2.topics.qrels"
@@ -72,11 +75,11 @@ object Application1 extends App
     })
 
 
-  // Need to count in how many documents each category has.
+  // Need to count how many documents each category has.
   private val doc_plurality_by_categories = categories_documents
     // attach count 1
     .map(cat_doc => (cat_doc._1, 1))
-    // reduce (sum)
+    // reduce by adding the ones... A.K.A. (sum)
     .reduceByKey(_+_)
 
 
@@ -92,7 +95,10 @@ object Application1 extends App
       // split each line into contents
       val split = line.split("\\s+")
 
+      // split contains document_id with a list of terms:weights pairs
+      // further split the terms weights
       val terms_weights = split.tail.map(_.split(":"))
+      // keep only term id and doc id  for all (flattening this will result in only (termid, docid) pairs
       terms_weights.map(terms_weights => (terms_weights(0), split.head))
 
     })
@@ -127,11 +133,16 @@ object Application1 extends App
     .map(l => (l._1, l._2.map(t => t._1)))
     // join (with document as key)
     .join(documents_grouped_by_cat)
+    // this holds (doc id as key, iterable terms and iterable categories)
+    // flatmap so that pairs of (term id, category) are mapped with 1 so that they can be counted with reduce later
+    // map from each entry the term to each other category and add the 1. Do this for all the terms of each entry.
     .flatMap(e => e._2._1.flatMap(t => e._2._2.map(c => ((t, c), 1))))
+    // reduce by adding the ones for the matching pairs.
     .reduceByKey(_ + _)
 
 
-
+  // reading the last file which will connect the term id to a stem
+  // hold (term, stem)
   private val terms_stems = spark.sparkContext.textFile(terms_stem_path)
     .map(line => {
       val split = line.split("\\s+")
@@ -150,14 +161,31 @@ object Application1 extends App
 //    .flatMap(term => collected_cats.map(c => (term._1, c._1)))
 //
 
+  // Need to create a pseudo "cartesian" product in order
+  // to pair up all the categories with all the terms.
+
+  // The categories are way less in amount
+  // map "1" as key and group by it
+  // In essence make the RDD horizontal not vertical...
+
+  // doc_plurality_by_cats is (category, #documents)
   private val category_list = doc_plurality_by_categories
+    // remap as 1 as key and category
     .map(c => (1, c._1 ))
+    // group with 1 key, hold (1, Seq((1, category)*)
     .groupBy(_._1)
+    // "essentially"make it horizontal
+    // keep key 1 and a list of only the categories
+    // (1, List(category*))
     .map(c => (1, c._2.map(x => x._2)))
 
+  // Now apply the above horizontal categories with 1 as key to the terms so that they get paired
   private val term_category = doc_plurality_by_terms
+    // remap with 1 as the key
     .map(t => (1, t._1))
+    // join upon 1
     .join(category_list)
+    // flatten each entry so that (term, category) is left
     .flatMap(e => e._2._2.map(x => (e._2._1, x)))
 
 
@@ -173,7 +201,7 @@ object Application1 extends App
   // intersected documents 173943 (only for pt3 file)
 
 
-
+  // Calculations
   private val res_triads = term_category
     // join stems upon terms
     .join(terms_stems)
@@ -195,19 +223,26 @@ object Application1 extends App
     // can join with intersected.
     .join(intersection_docs_with_cat_term)
     .map({
+      // match our entry's state
       case ((_, category),((stem, dT_count, dC_count), dC_dT_count)) =>
 
+        // calculate the jaccard index, by part
         val denominator = dT_count + dC_count - dC_dT_count
         val jaccard_index = if (denominator == 0) 0.0 else dC_dT_count.toDouble / denominator.toDouble
 
         (category, stem, jaccard_index)
     })
+    // remap to desired output format
     .map(e => s"<${e._1}>;<${e._2}>;<${e._3}>")
 
 //  res_triads.take(10).foreach(println)
 
+
+  // save to output path
   res_triads.saveAsTextFile(outputPath)
 
+
+  // terminate spark session
   spark.stop()
 
 }
--- Application2.scala
+++ Application2.scala
@@ -5,10 +5,9 @@ import org.apache.spark.sql.functions._
 import scala.math.Ordering.Float
 
 
-object Application2 extends App
-{
+object Application2 extends App {
   private val spark = SparkSession.builder.master("local[*]")
-    .appName(("Application Scenario 2: Vessel Trajectory Analytics in the Aegean Sea"))
+    .appName("Application Scenario 2: Vessel Trajectory Analytics in the Aegean Sea")
     .getOrCreate()
 
 
@@ -18,13 +17,11 @@ object Application2 extends App
   private val application2_path = hdfspath + "/nmea_aegean"
   private val nmea_aegean_logs_path = application2_path + "/nmea_aegean.logs"
 
-  private val Q1_outputPath = hdfspath + "/results/application2/Q1.csv"
-  private val Q2_outputPath = hdfspath + "/results/application2/Q2.csv"
-  private val Q3_outputPath = hdfspath + "/results/application2/Q3.csv"
-  private val Q4_outputPath = hdfspath + "/results/application2/Q4.csv"
-  private val Q5_outputPath = hdfspath + "/results/application2/Q5.csv"
-
-
+  private val Q1_outputPath = hdfspath + "/results/application2/Q1"
+  private val Q2_outputPath = hdfspath + "/results/application2/Q2"
+  private val Q3_outputPath = hdfspath + "/results/application2/Q3"
+  private val Q4_outputPath = hdfspath + "/results/application2/Q4"
+  private val Q5_outputPath = hdfspath + "/results/application2/Q5"
 
 
   private val logs = spark.read.option("header", "true").csv(nmea_aegean_logs_path)
@@ -32,25 +29,32 @@ object Application2 extends App
   //logs.show(10)
 
   // Q1: What is the number of tracked vessel positions per station per day?
-  // I guess we should check for empty rows
+
+
+
 
   private val no_vessels_per_day_per_station = logs
+    // Filtering for null data
     .filter(col("mmsi").isNotNull)
-    .filter((col("station").isNotNull && col("timestamp").isNotNull))
+    .filter(col("station").isNotNull && col("timestamp").isNotNull)
+    //Concating longtitude and latitude to create collumnt "tracked_position"
     .withColumn("tracked_position", concat_ws(",", col("longitude"), col("latitude")))
+    //Dropping the unnecessary columns
     .drop("longitude", "latitude")
+    //Since we need a per day per station result we groupBy accordingly
     .groupBy(col("station"), to_date(col("timestamp")))
+    //We search for unique vessel positions
     .agg(
       countDistinct("tracked_position").as("unique_tracked_pos")
     )
     .withColumnRenamed("count", "no_vessels_per_day_station")
     .withColumnRenamed("to_date(timestamp)", "date")
 
-  no_vessels_per_day_per_station
-    .write
-    .mode(SaveMode.Overwrite)
-    .format("csv")
-    .save(Q1_outputPath)
+  //  no_vessels_per_day_per_station
+  //    .write
+  //    .mode(SaveMode.Overwrite)
+  //    .format("csv")
+  //    .save(Q1_outputPath)
 
 
   //no_vessels_per_day_per_station.show(10)
@@ -60,28 +64,30 @@ object Application2 extends App
   //println(logs.filter((col("station").isNotNull && col("station").isNotNull)).count())
 
 
-
-
   // Q2: What is the vessel id with the highest number of tracked positions?
   // should count vessels with highest amount of unique longitude, latitude pairs
   // "tracked positions" should indicate unique pairs? (long, lat)
   private val highest_tracked_vessel = logs
     .filter(col("mmsi").isNotNull)
     .filter(col("longitude").isNotNull && col("latitude").isNotNull)
+    //We add collumn track_position and concat longtitude and latitude same as Q1
     .withColumn("tracked_position", concat_ws(",", col("longitude"), col("latitude")))
     .drop("longitude", "latitude")
+    //We groupBy Vessel ID since that is the what we are looking for
     .groupBy("mmsi")
+    //We get the count of distinct positions and order them by descending order to find the highest
     .agg(
       countDistinct("tracked_position").as("no_tracked_positions")
     )
     .orderBy(desc("no_tracked_positions"))
+    //we use limit(1) to keep only the first result
     .limit(1)
 
-  highest_tracked_vessel
-    .write
-    .mode(SaveMode.Overwrite)
-    .format("csv")
-    .save(Q2_outputPath)
+  //  highest_tracked_vessel
+  //    .write
+  //    .mode(SaveMode.Overwrite)
+  //    .format("csv")
+  //    .save(Q2_outputPath)
 
   //highest_tracked_vessel.show()
 
@@ -92,7 +98,6 @@ object Application2 extends App
   // 4000293
 
 
-
   // Q3: What is the average SOG of
   //                  vessels that appear in both station 8006 and station 10003
   //                                                             in the same day
@@ -100,53 +105,56 @@ object Application2 extends App
   private val station2 = "10003"
 
   private val unique_stations_per_day_per_vessel_amount = logs
+
     .filter(col("timestamp").isNotNull && col("station").isNotNull)
     .filter(col("speedoverground").isNotNull)
-
+    //Keeping only the entries that contain station1 and station2
     .filter(col("station") === station1 || col("station") === station2)
+    //grouping by Vessel ID and Date
     .groupBy(col("mmsi"), to_date(col("timestamp")).as("date"))
-
+    //collecting the set of (stations) per day and vessel, NOTE: sets contain only unique items, so each station will be represented only once.
     .agg(
       size(collect_set(col("station"))).as("unique_stations_per_day_per_vessel_amount")
     )
     .orderBy(desc("unique_stations_per_day_per_vessel_amount"))
-
+    //All the sets that have a size of 2 entries mean that these are vessels that appeared in both stations the same day
     .filter(col("unique_stations_per_day_per_vessel_amount") === 2)
 
-//  unique_stations_per_day_per_vessel_length.show()
+  //  unique_stations_per_day_per_vessel_length.show()
 
-
+  //Calculating avg_SOG
   private val avg_SOG_vessels_inStation8006and10003atSameDay = logs
-    // right join
-    .join(unique_stations_per_day_per_vessel_amount,  "mmsi")
+    // right join the main data set with the result of the previous processing
+    .join(unique_stations_per_day_per_vessel_amount, "mmsi")
+    //for the vessels that appeared in both stations the same day
     .groupBy("mmsi")
+    //calculating the avg_SOG
     .agg(
       avg(col("speedoverground").cast("float")).as(s"avg_SOG_per_vessel_in${station1}and${station2}InSameDay")
     )
   //avg_SOG_vessels_inStation8006and10003atSameDay.show()
 
-  avg_SOG_vessels_inStation8006and10003atSameDay
-    .write
-    .mode(SaveMode.Overwrite)
-    .format("csv")
-    .save(Q3_outputPath)
-
-
+  //  avg_SOG_vessels_inStation8006and10003atSameDay
+  //    .write
+  //    .mode(SaveMode.Overwrite)
+  //    .format("csv")
+  //    .save(Q3_outputPath)
 
 
   // Q4: What is the average Abs (Heading - COG) per station?
   private val avg_heading_minus_cog_per_station = logs
+    .filter(col("station").isNotNull)
     .groupBy("station")
     .agg(
       avg(abs(col("heading").cast("float") - col("courseoverground").cast("float"))).as("avg_deviationOfVessels_per_station")
     )
 
-  //avg_heading_minus_cog_per_station.show()
-  avg_heading_minus_cog_per_station
-    .write
-    .mode(SaveMode.Overwrite)
-    .format("csv")
-    .save(Q4_outputPath)
+  avg_heading_minus_cog_per_station.show()
+  //  avg_heading_minus_cog_per_station
+  //    .write
+  //    .mode(SaveMode.Overwrite)
+  //    .format("csv")
+  //    .save(Q4_outputPath)
 
 
   // Q5: What are the Top-3 most frequent vessel statuses?
@@ -156,13 +164,14 @@ object Application2 extends App
     .count()
     .withColumnRenamed("count", "vessel_amount")
     .orderBy(desc("vessel_amount"))
+    //keeping the first 3 results
     .limit(3)
 
-  top3_most_frequent_statuses
-    .write
-    .mode(SaveMode.Overwrite)
-    .format("csv")
-    .save(Q5_outputPath)
+  //  top3_most_frequent_statuses
+  //    .write
+  //    .mode(SaveMode.Overwrite)
+  //    .format("csv")
+  //    .save(Q5_outputPath)
 
   //top3_most_frequent_statuses.show()
 
@@ -173,31 +182,30 @@ object Application2 extends App
   // 4000293
 
   //
-//  - timestamp: the timestamp of the acquired vessel position
-//  - station: the id of the station which acquired the corresponding vessel position
-//  - mmsi: maritime mobile service identity, consider this as the vessel Id
-//  - longitude: geographic longitude of each timestamped vessel position
-//  - latitude: geographic latitude of each timestamped vessel position
-//  - Speed Over Ground (SOG): is the speed of the vessel relative to the surface of the earth
-//  - Course Over Ground (COG): is the actual direction of progress of a vessel, between two
-//  points, with respect to the surface of the earth
-//  - Heading: is the compass direction in which the vessel bow is pointed
-//  - Status:
+  //  - timestamp: the timestamp of the acquired vessel position
+  //  - station: the id of the station which acquired the corresponding vessel position
+  //  - mmsi: maritime mobile service identity, consider this as the vessel Id
+  //  - longitude: geographic longitude of each timestamped vessel position
+  //  - latitude: geographic latitude of each timestamped vessel position
+  //  - Speed Over Ground (SOG): is the speed of the vessel relative to the surface of the earth
+  //  - Course Over Ground (COG): is the actual direction of progress of a vessel, between two
+  //  points, with respect to the surface of the earth
+  //  - Heading: is the compass direction in which the vessel bow is pointed
+  //  - Status:
 
-//  o 0 = underway using engine
-//  o 1 = at anchor
-//  o 2 = not under command
-//  o 3 = restricted maneuverability
-//  o 4 = constrained by her draught
-//  o 5 = moored
-//  o 6 = aground
-//  o 7 = engaged in fishing
-//  o 8 = underway sailing
-//  o 9 & 10 = reserved for future amendment
-//  o 11 = power-driven vessel towing astern
-//  o 12 = power-driven vessel pushing ahead or towing alongside
-//  o 13 = reserved for future use
-//  o 14 = AIS-SART Active (Search and Rescue Transmitter), AIS-MOB (Man Overboard), AISEPIRB (Emergency Position Indicating Radio Beacon):
-//  o 15 = undefined = default (also used by AIS-SART, MOB-AIS, and EPIRB-AIS under test).
-
+  //  o 0 = underway using engine
+  //  o 1 = at anchor
+  //  o 2 = not under command
+  //  o 3 = restricted maneuverability
+  //  o 4 = constrained by her draught
+  //  o 5 = moored
+  //  o 6 = aground
+  //  o 7 = engaged in fishing
+  //  o 8 = underway sailing
+  //  o 9 & 10 = reserved for future amendment
+  //  o 11 = power-driven vessel towing astern
+  //  o 12 = power-driven vessel pushing ahead or towing alongside
+  //  o 13 = reserved for future use
+  //  o 14 = AIS-SART Active (Search and Rescue Transmitter), AIS-MOB (Man Overboard), AISEPIRB (Emergency Position Indicating Radio Beacon):
+  //  o 15 = undefined = default (also used by AIS-SART, MOB-AIS, and EPIRB-AIS under test).
 }
